d42082a09349c595fb54c55011696c7c82a4f79b
Taki Choufa <taki@example.com>
2025-12-01 21:56:04 -0500

    Project Colors

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class JunkieColors {
+  static const background = Color(0xFF1A1A1A);
+  static const card = Color(0xFF242424);
+  static const accent = Color(0xFFE55A1C); // orange
+  static const text = Colors.white;
+  static const subtext = Colors.white70;
+}   
